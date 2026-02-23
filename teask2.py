915e4af940367bca286a17b11ddd3ddcd72c77f3
AsadUllah <asadullahnoor698@gmail.com>
2026-02-23 16:08:14 +0500

    Initial commit

---
+++ teask2.py
@@ -0,0 +1,4 @@
+input_number=int(input("Enter the number here:__________________"))
+if input_number%2==0:
+      print(f"The number {input_number} is even")
+else:      print(f"The number {input_number} is odd")
