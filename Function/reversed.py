6562f506702a469096836e736b5e5c9148217817
AsadUllah <asadullahnoor698@gmail.com>
2026-02-24 18:38:22 +0500

    Add the reversed function

---
+++ Function/reversed.py
@@ -0,0 +1,8 @@
+def reversed_string(string):
+   reversed_str=string[::-1]
+   return reversed_str
+def main_function():
+    print("i m making the Reversed String Function")
+    input_string=input("Enter the String here :")
+    print("The reversed string is:", reversed_string(input_string))
+main_function()
