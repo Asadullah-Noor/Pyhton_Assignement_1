f42432ca5e1e4e1869ef7b6dad4e9a23e1e098eb
AsadUllah <asadullahnoor698@gmail.com>
2026-02-25 19:31:35 +0500

    update

--- Function/calculate.py
+++ Function/calculate.py
@@ -1,5 +1,13 @@
 def calculate_total(price,quantity):
     total=price*quantity
     return total
-
-print(calculate_total(10,5))
+def discount_price(calculate_total,discount):
+    discount_amount=(calculate_total*discount)/100
+    final_price=calculate_total-discount_amount
+    return final_price
+print("Total Price :",calculate_total(10,5))
+print("Discounted Price :",discount_price(calculate_total(10,5),5))
+def temprature_conversion(celsius):
+    fahrenheit=(celsius*9/5)+32
+    return fahrenheit
+print(temprature_conversion(25))
