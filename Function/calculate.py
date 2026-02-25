076e35cd6344d95d921d6af981cfd4d159e6a5f3
AsadUllah <asadullahnoor698@gmail.com>
2026-02-25 23:23:59 +0500

    Update

--- Function/calculate.py
+++ Function/calculate.py
@@ -11,3 +11,22 @@ def temprature_conversion(celsius):
     fahrenheit=(celsius*9/5)+32
     return fahrenheit
 print(temprature_conversion(25))
+
+
+# Grade Calculator
+def grade_calculator(*marks):
+    total_marks=sum(marks)
+    average_marks=total_marks/len(marks)
+    print("Total Marks :",total_marks)
+    print("Average Marks :",average_marks)
+    if total_marks>=90:
+        return "A"
+    elif total_marks>=80:
+        return "B"
+    elif total_marks>=70:
+        return "C"
+    elif total_marks>=60:
+        return "D"
+    else:
+        return "F"
+print("Grade :",grade_calculator(85,23,4,5,2,5,25,5,2,5,25,5,2,5,25,5,2,5,25,5,2,5,25))
