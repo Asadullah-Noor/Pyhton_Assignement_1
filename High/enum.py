685158a5eee2d2dd611938b0a5eda86c9a6a8a0d
AsadUllah <asadullahnoor698@gmail.com>
2026-02-27 02:11:01 +0500

    update overal code

---
+++ High/enum.py
@@ -0,0 +1,3 @@
+fruits=["Apple","Banana","Cherry"]
+for index, fruits in enumerate(fruits):
+    print(f"Index: {index}, Fruit: {fruits}")
---
+++ High/zip.py
@@ -0,0 +1,5 @@
+name=["Asadullah","Noor","Asad"]
+passion=["Python","AI","ML"]
+def zip_lists(list1,list2):
+    return list(zip(list1,list2))
+print(zip_lists(name,passion))
