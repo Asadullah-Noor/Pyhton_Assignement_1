076cd5a5eff645539688e5f6684af0476ed82c70
AsadUllah <asadullahnoor698@gmail.com>
2026-03-02 03:31:03 +0500

    update the file handling code

--- file_handling/t1.py
+++ file_handling/t1.py
@@ -1,2 +1,9 @@
-with open('open.txt','w') as file:
-    file.write("My name is Asadullah Today Ayatullah Khumnai Have been Died")
+# with open('open.txt','w') as file:
+#     file.write("My name is Asadullah Today Ayatullah Khumnai Have been Died")
+count=0
+with open('open.txt','r')as f:
+ 
+  for i in f:
+   count+=1
+print(f.readlines())
+print("Total line is",count)
