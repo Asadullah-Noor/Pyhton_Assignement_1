5effd25b09f6c072f3af94b994b6529295bff504
AsadUllah <asadullahnoor698@gmail.com>
2026-03-10 01:32:48 +0500

    Updat ethe codee

--- oop/Project.py
+++ oop/Project.py
@@ -1,6 +1,6 @@
 from abc import ABC,abstractmethod
 class Bank(ABC):
-    def __init__(self,balance,owner,accountNumber):
+    def __init__(self,accountNumber,owner,balance):
         self.__accountNumber=accountNumber
         self.__owner=owner
         self.__balance=balance
@@ -17,14 +17,50 @@ class Bank(ABC):
         print(f"The total Balance is {self.__balance}")
         print("If you interested to deposit the amount please check it again")
 class SavingsAccount(Bank):
-    print("Hello I m Asadullah Noor") 
-    def deposit(self,amount):
-        if amount <=0 or amount>10000:
-            print("Invalid Amount")
+    def deposit(self, amount):
+        return super().deposit(amount)
+    MIN_Balance=500
+    def withdraw(self, amount):
+        if self._Bank__balance - amount<self.MIN_Balance:
+            print("Withdrawals are not allowed if the balance would drop below this minimum.")
         else:
-            self._Bank__balance+=amount
-            return self._Bank__balance
-
-    
+            self._Bank__balance-=amount
+            print(f"Withdrawn: {amount}")
+            print(f"New Balance: {self.balance}")
+class CurrentAccount(Bank):
+    overdraft=-1000
+    def deposit(self, amount):
+        return super().deposit(amount)
+    def withdraw(self,amount):
+        if self._Bank__balance - amount<self.overdraft:
+            print("Allows an overdraft of up to -1000.")
+        else:
+            self._Bank__balance -=amount
+            print(f"Withdrawn: {amount}")
+            print(f"New Balance: {self.balance}")
+class FixedDepositAccount(Bank):
+    def __init__(self,accountNumber,owner,balance,matured=False):
+        super().__init__(accountNumber,owner,balance)
+        self.matured=matured
+    def deposit(self, amount):
+        return super().deposit(amount)
+    def withdraw(self, amount):
+        if not self.matured:
+            print("Withdrawal denied! Account has not matured yet.")
+        else:
+            if amount <= self._Bank__balance:
+                self._Bank__balance -= amount
+                print(f"Withdrawn: {amount}")
+                print(f"New Balance: {self.balance}")
+            else:
+                print("Insufficient balance")
+class Encapsulate_cls(Bank):
+    balance=0
+    def deposit(self, amount):
+        return super().deposit(amount)
+    def get_balance(self,):
+        return self._Bank__balance
+    def set_balance(self, value):
+        self._Bank__balance = value
 account=SavingsAccount(20000, "Asadullah" , "ls23bsse0103") 
-account.display_account_info()
+print(account.deposit(2000))
