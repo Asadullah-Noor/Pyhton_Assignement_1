aa2b7710bda3ee01874d94b09c82727d3aa4dd39
AsadUllah <asadullahnoor698@gmail.com>
2026-03-27 00:25:50 +0500

    fourth

--- oop/Project/Ride.py
+++ oop/Project/Ride.py
@@ -1,36 +1,97 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
+
+# --- Custom Exceptions ---
 class NoDriverAvailableException(Exception):
     pass
+
 class DriverAlreadyBookedException(Exception):
     pass
-class User(ABC):
-    def __init__(self,user,Start_location,End_location):
-        self.user=user
-        self.Start_location=Start_location
-        self.End_location=End_location
-    def Request_Ride(self,User):
+
+
+# --- Fare Strategy (Abstraction) ---
+class FareStrategy(ABC):
+    @abstractmethod
+    def calculate_fare(self, distance):
         pass
-    def Assign_Driver(self):
-        pass
-    def CalculatePrice(self):
-        if not self.Start_location || not self.End_location:
-            raise f"Please enter the both starting and engding location"
-        if self.Start_location && self.End_location:
-            total_Price=20*(self.Start_location + self.End_location)
-        else:
-            raise ValueError("Please Enter the Valid location here")
-class Driver(User):
-     def Assign_Driver(self,user,Driver):
-        if not self.user
-           raise Exception("User is not available so we could not add the Driver")
-        if self.user:
-            return f"The User {self.user} assign Driver is {self.Driver}"
+
+
+class StandardFare(FareStrategy):
+    def calculate_fare(self, distance):
+        return 20 * distance
+
+
+# --- User Class ---
+class User:
+    def __init__(self, name, start_location, end_location):
+        self.name = name
+        self.start_location = start_location
+        self.end_location = end_location
+
+    def request_ride(self, ride_manager):
+        return ride_manager.create_ride(self)
+
+
+# --- Driver Class ---
+class Driver:
+    def __init__(self, name):
+        self.name = name
+        self.available = True
+
+
+# --- Ride Class ---
 class Ride:
-    def RequestRide(self):
-        if not self.user:
-            raise Exception("User is not Available")
-        if self.user:
-            print("Request for the Rider")
+    def __init__(self, user, driver, distance, fare_strategy):
+        self.user = user
+        self.driver = driver
+        self.distance = distance
+        self.fare = fare_strategy.calculate_fare(distance)
+
+    def ride_details(self):
+        return f"User: {self.user.name}, Driver: {self.driver.name}, Fare: {self.fare}"
 
 
+# --- Ride Manager ---
 class RideManager:
+    def __init__(self):
+        self.drivers = []
+
+    def add_driver(self, driver):
+        self.drivers.append(driver)
+
+    def find_driver(self):
+        for driver in self.drivers:
+            if driver.available:
+                return driver
+        raise NoDriverAvailableException("No drivers available")
+
+    def create_ride(self, user):
+        if not user.start_location or not user.end_location:
+            raise ValueError("Invalid locations")
+
+        driver = self.find_driver()
+
+        if not driver.available:
+            raise DriverAlreadyBookedException("Driver already booked")
+
+        # Assign driver
+        driver.available = False
+
+        # Example: distance calculation (simple)
+        distance = abs(user.end_location - user.start_location)
+
+        ride = Ride(user, driver, distance, StandardFare())
+
+        return ride
+manager = RideManager()
+
+d1 = Driver("Ali")
+d2 = Driver("Ahmed")
+
+manager.add_driver(d1)
+manager.add_driver(d2)
+
+user = User("Asadullah", 2, 10)
+
+ride = user.request_ride(manager)
+
+print(ride.ride_details())
